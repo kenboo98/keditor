a4007096ac83d8070224ed785256232d8a28f2be
kenboo98 <tellambu@ualberta.ca>
2020-06-06 17:19:38 -0600

    Fixed up cursor movement

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use std::io;
 // Import the color module.
 use termion::{clear, cursor};
 use termion::event::Key;
-use termion::input::TermRead;
+use termion::input::{TermRead, Keys};
 use termion::raw::{IntoRawMode, RawTerminal};
 
 enum CursorDir {
@@ -17,24 +17,64 @@ enum CursorDir {
     Right,
 }
 
-fn move_cursor(std_out: &mut RawTerminal<Stdout>, dir: CursorDir) {
-    match dir {
-        CursorDir::Left => {
-            write!(std_out, "{}",
-                   termion::cursor::Left(1)).unwrap();
+struct IntermediateFile {
+    lines: Vec<String>
+}
+
+struct Editor {
+    intermediate_file: IntermediateFile,
+    cursor_col: u16,
+    cursor_row: u16,
+}
+
+impl Editor {
+    fn new(file: IntermediateFile) -> Editor {
+        Editor {
+            intermediate_file: file,
+            cursor_col: 1,
+            cursor_row: 1,
         }
-        CursorDir::Right => {
-            write!(std_out, "{}",
-                   termion::cursor::Right(1)).unwrap();
+    }
+
+    fn move_cursor(&mut self, dir: Key) {
+        match dir {
+            Key::Left => {
+                if self.cursor_col > 1 {
+                    self.cursor_col -= 1
+                }
+            }
+            Key::Right => {
+                if self.intermediate_file.lines[self.cursor_row as usize - 1].len() >= self.cursor_col as usize {
+                    self.cursor_col += 1
+                } else if self.cursor_row < self.intermediate_file.lines.len() as u16 {
+                    self.cursor_col = 0;
+                    self.cursor_row += 1;
+                }
+            }
+            Key::Down => {
+                if self.cursor_row < self.intermediate_file.lines.len() as u16 {
+                    self.cursor_row += 1
+                }
+            }
+            Key::Up => {
+                if self.cursor_row > 1 {
+                    self.cursor_row -= 1
+                }
+            }
+            _ => {}
         }
-        CursorDir::Down => {
-            write!(std_out, "{}",
-                   termion::cursor::Down(1)).unwrap();
+    }
+
+    fn print_lines(&mut self, stdout: &mut RawTerminal<Stdout>) {
+        write!(stdout, "{}", termion::cursor::Goto(1, 1)).unwrap();
+        for (i, line) in self.intermediate_file.lines.iter().enumerate() {
+            write!(stdout, "{}{}", line, termion::cursor::Goto(1, (i + 2) as u16));
         }
-        CursorDir::Up => {
-            write!(std_out, "{}",
-                   termion::cursor::Up(1)).unwrap();
-        }
+        write!(stdout, "{}", termion::cursor::Goto(self.cursor_col, self.cursor_row)).unwrap();
+    }
+
+    fn clear(&mut self, stdout: &mut RawTerminal<Stdout>) {
+        write!(stdout, "{}", termion::clear::All);
     }
 }
 
@@ -60,44 +100,56 @@ pub fn run(mut args: std::env::Args) -> Result<(), &'static str> {
                 }
                 buffer.push(line);
             }
-            _ => {break;}
+            _ => { break; }
         }
     }
+    let intermediate_file = IntermediateFile { lines: buffer };
+
+    let mut editor = Editor::new(intermediate_file);
 
 
     let mut stdout = stdout().into_raw_mode().unwrap();
-
     write!(stdout, "{}{}",
            // Clear the screen.
            termion::clear::All,
            // Goto (1,1).
            termion::cursor::Goto(1, 1)).unwrap();
 
-    for (i, line) in buffer.iter().enumerate() {
-        write!(stdout, "{}{}", line, termion::cursor::Goto(1, (i+2) as u16));
 
+    for (i, line) in editor.intermediate_file.lines.iter().enumerate() {
+        write!(stdout, "{}{}", line, termion::cursor::Goto(1, (i + 2) as u16));
     }
+    write!(stdout, "{}", termion::cursor::Goto(editor.cursor_col, editor.cursor_row)).unwrap();
+
+    stdout.flush().unwrap();
 
 
-    loop {
-        let stdin = stdin();
-        for c in stdin.keys() {
-            // Print the key we type...
-            match c.unwrap() {
-                // Exit.
-                Key::Char(c) => print!("{}", c),
-                Key::Alt(c) => print!("Alt-{}", c),
-                Key::Ctrl('c') => { return Ok(()); }
-                Key::Left => move_cursor(&mut stdout, CursorDir::Left),
-                Key::Right => move_cursor(&mut stdout, CursorDir::Right),
-                Key::Up => move_cursor(&mut stdout, CursorDir::Up),
-                Key::Down => move_cursor(&mut stdout, CursorDir::Down),
-                Key::Backspace => print!("\r"),
-                _ => print!("Other"),
+    let stdin = stdin();
+
+    for c in stdin.keys() {
+        // Print the key we type...
+        match c.unwrap() {
+            // Exit.
+            Key::Char(c) => print!("{}", c),
+            Key::Alt(c) => print!("Alt-{}", c),
+            Key::Ctrl('c') => {
+                editor.clear(&mut stdout);
+                return Ok(());
             }
-
-            // Flush again.
-            stdout.flush().unwrap();
+            Key::Left => editor.move_cursor(Key::Left),
+            Key::Right => editor.move_cursor(Key::Right),
+            Key::Up => editor.move_cursor(Key::Up),
+            Key::Down => editor.move_cursor(Key::Down),
+            _ => print!("Other"),
         }
+
+
+        write!(stdout, "{}",
+               // Clear the screen.
+               termion::clear::All).unwrap();
+
+        editor.print_lines(&mut stdout);
+        stdout.flush().unwrap();
     }
+    Ok(())
 }
